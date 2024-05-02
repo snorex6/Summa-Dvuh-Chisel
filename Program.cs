20e575950f6537183d2f9ade80f725dd0e0b37ba
name <name@DESKTOP-CK44DBL>
2024-05-02 15:41:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Введите число а");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма равна {a+b}");
